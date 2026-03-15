36d040bf3ab2bb964632b7ff8a4c8718b814c454
reyesmma <reyesmma@mail.uc.edu>
2026-03-15 18:49:55 -0400

    Task 07 Complete

---
+++ src/Task07.java
@@ -0,0 +1,17 @@
+public class Task07
+{
+    public static void main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                System.out.print('*' + " ");
+            }
+            if(i<4)
+            {
+                System.out.print("\n");
+            }
+        }
+    }
+}
